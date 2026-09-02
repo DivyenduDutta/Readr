de344209ac9860fafbc85d51f8ae0a1a2da7d881
Divyendu Dutta <connect2divyendu@gmail.com>
2026-09-03 00:03:37 +0530

    [FEAT] Add simple script to test
    Added simple script to test google gemini api key usage

--- src/readr/__init__.py
+++ src/readr/__init__.py
@@ -1,2 +1,14 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
 def main() -> None:
-    print("Hello from readr!")
+    client = genai.Client(api_key=os.getenv("GOOGLE_GEMINI_API_KEY"))
+    response = client.models.generate_content(
+        model="gemini-3.6-flash",
+        contents="why is the sky blue?"
+    )
+    print(response.text)
+    client.close()
