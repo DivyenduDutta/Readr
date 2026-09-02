fdeb7a724ab2208c36e83ea25dc3577b1b7e85e9
Divyendu Dutta <connect2divyendu@gmail.com>
2026-09-03 00:16:22 +0530

    [FEAT] Fetch sample HN article
    Fetched and downloaded sample HN article using trafilature

--- src/readr/__init__.py
+++ src/readr/__init__.py
@@ -1,14 +1,20 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from trafilatura import fetch_url, extract
 
 load_dotenv()
 
+URL = "https://ploum.net/2026-09-02-i_dont_have_a_smartphone.html"
+
 def main() -> None:
-    client = genai.Client(api_key=os.getenv("GOOGLE_GEMINI_API_KEY"))
-    response = client.models.generate_content(
-        model="gemini-3.6-flash",
-        contents="why is the sky blue?"
-    )
-    print(response.text)
-    client.close()
+    #client = genai.Client(api_key=os.getenv("GOOGLE_GEMINI_API_KEY"))
+    #response = client.models.generate_content(
+        #model="gemini-3.6-flash",
+        #contents="why is the sky blue?"
+    #)
+    #print(response.text)
+    #client.close()
+    page = fetch_url(URL)
+    text = extract(page)
+    print(text)
