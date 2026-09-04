b188da9c2deafacb6fc009a316e47d9a69082d34
Divyendu Dutta <connect2divyendu@gmail.com>
2026-09-04 23:53:41 +0530

    [CHORE] Add yaml config file
    Added config file for readr applcation
    Removed constants file. Managing via config file

--- src/readr/constants/file_constants.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class FileConstants(Enum):
-    """
-    Enum class to hold file constants.
-    """
-    SYSTEM_INSTRUCTION_PROMPT_FILE_NAME = "system_instruction_prompt.md"
