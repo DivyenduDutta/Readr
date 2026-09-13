3e1bea0f0aee5f9135d257c58eca518b4f159d8f
Divyendu Dutta <connect2divyendu@gmail.com>
2026-09-13 13:08:04 +0530

    [CHORE] Update tool class
    Updated article fetcher tool class to return meta information regarding the tool.

--- src/readr/tools/ArticleFetcherTool.py
+++ src/readr/tools/ArticleFetcherTool.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from trafilatura import extract, fetch_url
 
 from readr.constants.tool_constants import ToolDescriptions, ToolNames
@@ -10,8 +12,6 @@ class ArticleFetcherTool(metaclass=SingletonMeta):
     """
 
     def __init__(self):
-        self.tool_name = ToolNames.ARTICLE_FETCHER.value
-        self.tool_description = ToolDescriptions.ARTICLE_FETCHER_DESC.value
         self.url = None
         self.article_content = None
 
@@ -22,3 +22,22 @@ class ArticleFetcherTool(metaclass=SingletonMeta):
         page = fetch_url(self.url)
         self.article_content = extract(page)
         return self.article_content if self.article_content is not None else ""
+
+    @staticmethod
+    def get_tool_description() -> dict[str, Any]:
+        tool_desc = {
+            "type": "function",
+            "name": f"{ToolNames.ARTICLE_FETCHER.value}",
+            "description": f"{ToolDescriptions.ARTICLE_FETCHER_DESC.value}",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "url": {
+                        "type": "string",
+                        "description": "The URL to fetch the contents of.",
+                    },
+                },
+                "required": ["url"],
+            },
+        }
+        return tool_desc
